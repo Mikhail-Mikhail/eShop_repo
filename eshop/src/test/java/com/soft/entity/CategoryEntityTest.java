6c1a8ca3ab219f3b2c6c0388cf3288fb1678a360
Mikhail-Mikhail <mmbrdf@mail.ru>
2020-01-29 17:29:24 +0300

    ver1_29/01/2020 Unit testing in progress...

--- eshop/src/test/java/com/soft/entity/CategoryEntityTest.java
+++ eshop/src/test/java/com/soft/entity/CategoryEntityTest.java
@@ -11,9 +11,15 @@ import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
 //------------------------------------------------------------------------------
@@ -25,39 +31,49 @@ public class CategoryEntityTest {
    //Reference to instance of testing class.
    CategoryEntity ce;
    
-   static byte[] bArr;
+//   static byte[] bArr;
    
     //Execute before all tests.
     @BeforeAll
     public static void init() {       
-      bArr = new byte[2]; 
+//      bArr = new byte[2]; 
     }
 
     //Execute after all tests.
     @AfterAll
     public static void tearDown() {  
-      bArr = null;	
+//      bArr = null;	
     } 
 
      @Test
-     public void firstTest() {
-    	 	    
-	   ce = new CategoryEntity(1L, "Resistors", "ResistorsEntity", bArr);
-	   assertTrue(ce.equals(ce));
-       assertEquals(12,12);
+     public void annotationsTest() {
+    	 	   
+       //Test for presence of class's annotations.	 
+       TestHelper.assertClassAnnotations(CategoryEntity.class, Entity.class, Table.class, Cache.class);              
        
-//    	assertFalse(true, "Annots = "+CategoryEntity.class.getAnnotations().length);       
+        //Tests for presence of field's annotations.                     
+        TestHelper.assertFieldAnnotations(CategoryEntity.class, "id", Id.class, Column.class, GeneratedValue.class, GenericGenerator.class);
+        TestHelper.assertFieldAnnotations(CategoryEntity.class, "name", Column.class);
+        TestHelper.assertFieldAnnotations(CategoryEntity.class, "entityName", Column.class);
+        TestHelper.assertFieldAnnotations(CategoryEntity.class, "photo", Column.class);      
        
-  // assertAnnotations(CategoryEntity.class, "id", Id.class, GeneratedValue.class, Column.class);  
-       TestHelper.assertAnnotations(Arrays.asList(Entity.class, Table.class, Cache.class), Arrays.asList(CategoryEntity.class.getAnnotations()));             
-     }    
+         //Tests for presence of getter-method's annotations.                     
+         TestHelper.assertGetterAnnotations(CategoryEntity.class, "getId");                
+         TestHelper.assertGetterAnnotations(CategoryEntity.class, "getName");                 
+         TestHelper.assertGetterAnnotations(CategoryEntity.class, "getEntityName");                  
+         TestHelper.assertGetterAnnotations(CategoryEntity.class, "getPhoto");                  
+     }  
      
      @Test
-     public void secondTest() {
-    	 	    
-	   ce = new CategoryEntity(1L, "Resistors", "ResistorsEntity", bArr);
-       assertTrue(ce.equals(ce));     
-       assertEquals(12,12);  
-     }          
+     public void tableNameTest() {
+      
+       final String TABLE_NAME = "category";
+       
+        //Get annotation "@Table" for class. 
+        Table t = TestHelper.getClassAnnotation(CategoryEntity.class, Table.class);
+    
+         //Test table's name.
+         Assertions.assertEquals(TABLE_NAME, t.name(), String.format("TEST FAILURE: Parameter \"name\" of annotation \"@table\" = "+"\""+t.name()+"\" instead of "+"\""+TABLE_NAME+"\""));              
+     }  
 }
 
--- eshop/src/test/java/com/soft/helper/TestHelper.java
+++ eshop/src/test/java/com/soft/helper/TestHelper.java
@@ -3,7 +3,15 @@ package com.soft.helper;
 //------------------------------------------------------------------------------
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+
+import com.soft.entity.CategoryEntity;
 
 //------------------------------------------------------------------------------
 
@@ -12,24 +20,66 @@ import java.util.List;
 public class TestHelper {
 	
 	//Method to check presence of annotations. 
-	public static void assertAnnotations(List<Class> expectedAnnotationClasses, List<Annotation> annotations) {
+	public static void assertAnnotations(String testElementName, List<Class> expectedAnnotationClasses, List<Annotation> actualAnnotations) {
 		
        //Compare quantity of expected and actual annotations.
-	   if (expectedAnnotationClasses.size() != annotations.size()) {
-	     throw new AssertionError(String.format("Expected %d annotations, but found %d", expectedAnnotationClasses.size(), annotations.size()));	      
+	   if (expectedAnnotationClasses.size() != actualAnnotations.size()) {
+	     throw new AssertionError(String.format("TEST FAILURE: "+testElementName+". Expected %d annotations, but found %d .", expectedAnnotationClasses.size(), actualAnnotations.size()));	      
 	   }
 
         //Check presence of an expected annotation in the actual annotation's list.  
 	    expectedAnnotationClasses.forEach(
 
 	       ac -> {
-	        long cnt  = annotations.stream().filter(a -> a.annotationType().isAssignableFrom(ac)).count();	         	           	            
+	        long cnt  = actualAnnotations.stream().filter(a -> a.annotationType().isAssignableFrom(ac)).count();	         	           	            
 
 	          if (cnt == 0) {
-	            throw new AssertionError(String.format("No annotation of type %s found", ac.getName()));	          
+	            throw new AssertionError(String.format("TEST FAILURE: "+testElementName+". No annotation of type %s found.", ac.getName()));	          
 	          }
 	       }
 	    );
     }
+	
+	
+	  //Method to check class's annotations.
+	  //If class has no annotation third parameter "annotationClasses" should be omitted.
+	  public static void assertClassAnnotations(Class cls, Class... annotationClasses) {		 
+
+		 try {
+	        assertAnnotations("Class "+"\""+cls.getCanonicalName()+"\"", Arrays.asList(annotationClasses), Arrays.asList(cls.getAnnotations()));		        		        		    
+	     } 
+		 catch (Exception e) {
+	       throw new AssertionError(e);
+		 }
+	  }
+	
+  	  //Method to check field's annotations.
+	  //If field has no annotation third parameter "annotationClasses" should be omitted.
+	  public static void assertFieldAnnotations(Class c, String fieldName, Class... annotationClasses) {		 
+
+		 try {
+	        assertAnnotations("Field "+"\""+fieldName+"\"", Arrays.asList(annotationClasses), Arrays.asList(c.getDeclaredField(fieldName).getAnnotations()));		        		        		    
+	     } 
+		 catch (NoSuchFieldException nsfe) {
+ 	       throw new AssertionError(nsfe);
+		 }
+	  }
+	  
+  	  //Method to check getter-method's annotations.
+	  //If method has no annotation third parameter "annotationClasses" should be omitted.
+	  public static void assertGetterAnnotations(Class c, String methodName, Class...annotationClasses) {
+		     
+		 try {  
+		    assertAnnotations("Method "+"\""+methodName+"\"", Arrays.asList(annotationClasses), Arrays.asList(c.getDeclaredMethod(methodName).getAnnotations()));		        		        		     
+         } 
+  	     catch (NoSuchMethodException nsfe) {
+	        throw new AssertionError(nsfe);
+		 }
+	  }
+	  	  
+	    //Method to get class's annotation. 
+	    public static <T extends Annotation> T getClassAnnotation(Class<?> c, Class<T> annotation) {		
+		  return (T) c.getAnnotation(annotation);
+		}
 }
 //------------------------------------------------------------------------------
