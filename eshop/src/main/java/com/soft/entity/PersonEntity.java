40fde7df762b868a01c4f66f678c7ab590696f96
Mikhail-Mikhail <mmbrdf@mail.ru>
2020-02-07 15:14:18 +0300

    ver_07/02/2020 Auto creating of tables in HSQLDB in progress...

---
+++ eshop/src/main/java/com/soft/entity/PersonEntity.java
@@ -0,0 +1,47 @@
+package com.soft.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "person") 
+public class PersonEntity {
+
+	public PersonEntity(Long id, char name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Id    
+    @Column(name = "id")    
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment") 
+    private Long id;
+
+    @Column(name = "name")
+    private char name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public char getName() {
+		return name;
+	}
+
+	public void setName(char name) {
+		this.name = name;
+	}        
+}
